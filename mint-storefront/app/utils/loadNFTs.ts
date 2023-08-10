bb50b4170f0922dcadf6445bdc72829405612f95
Luke Cassady-Dorion <luke@SPStories.com>
2023-08-10 16:00:36 -0400

    Update loadNFTs.ts
    Updated formatting

--- mint-storefront/app/utils/loadNFTs.ts
+++ mint-storefront/app/utils/loadNFTs.ts
@@ -4,4 +4,6 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { fetchCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
 import { PublicKey, Pda } from "@metaplex-foundation/umi";
 
-export const loadNFTs = async (): Promise<string[]> => {};
+export const loadNFTs = async (): Promise<string[]> => {
+  
+};
