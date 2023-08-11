a98080da1459a0c10084784b3e521cef20d10063
Luke Cassady-Dorion <luke@SPStories.com>
2023-08-11 09:10:57 -0400

    added some comments

--- mint-storefront/app/utils/loadNFTs.ts
+++ mint-storefront/app/utils/loadNFTs.ts
@@ -1,9 +1,8 @@
-// utils/loadNFTs.ts
-
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { fetchCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
 import { PublicKey, Pda } from "@metaplex-foundation/umi";
 
-export const loadNFTs = async (): Promise<string[]> => {
-  
-};
+/**
+ * Loads preview images of all NFTs in the collection
+ */
+export const loadNFTs = async (): Promise<string[]> => {};
